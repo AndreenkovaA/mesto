e25f19d828a8c9216f38d7503dcae169ea08f823
Andreenkova Anastasiia <andreenkowaa@yandex.ru>
2023-01-18 20:40:17 +0300

    fix: починить адаптив

--- script/script.js
+++ script/script.js
@@ -13,36 +13,22 @@ function hideForm() {
 editButton.addEventListener('click', showForm);
 closeButton.addEventListener('click', hideForm);
 
-// Находим форму в DOM
 let formElement = document.querySelector('.popup__container');
-// Находим поля формы в DOM
 let submit = formElement.querySelector('.form__button-submit');
 let nameInputList = formElement.querySelectorAll('.form__item');
 let nameInput = nameInputList[0];
 let jobInput = nameInputList[1];
 
-// Обработчик «отправки» формы, хотя пока
-// она никуда отправляться не будет
 function handleFormSubmit (evt) {
-    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-                                                // Так мы можем определить свою логику отправки.
-                                                // О том, как это делать, расскажем позже.
-
-    // Получите значение полей jobInput и nameInput из свойства value
+  evt.preventDefault(); 
   let name = nameInput.value;
   let job = jobInput.value;
- 
-
-    // Выберите элементы, куда должны быть вставлены значения полей
   let profile = document.querySelectorAll('p');
   let profileName = profile[0];
   let profileText = profile[1];
-    // Вставьте новые значения с помощью textContent
   profileName.textContent = name;
   profileText.textContent = job;
   hideForm();
 }
 
-// Прикрепляем обработчик к форме:
-// он будет следить за событием “submit” - «отправка»
 formElement.addEventListener('submit', handleFormSubmit);
