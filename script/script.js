a14b41a388d27c7e6fefb56e4c397f5980ad25a9
Andreenkova Anastasiia <andreenkowaa@yandex.ru>
2023-01-29 22:48:24 +0300

    fix: добавлять карточки из js

--- script/script.js
+++ script/script.js
@@ -7,6 +7,62 @@ let jobInput = document.getElementById('job');
 let profileName = document.querySelector('.profile__name');
 let profileText = document.querySelector('.profile__text');
 
+const elements = document.querySelector('.elements');
+
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+initialCards.forEach(function (element) { 
+  const card = document.createElement('div');
+  card.classList.add('elements__element');
+
+  const cardPhoto = document.createElement('img');
+  cardPhoto.classList.add('elements__photo');
+  cardPhoto.src = element.link;
+  cardPhoto.alt = element.name;
+
+  const cardElements = document.createElement('div');
+  cardElements.classList.add('elements__group');
+
+  const cardTitle = document.createElement('h2');
+  cardTitle.classList.add('elements__title');
+  cardTitle.textContent = element.name;
+  
+  const cardBtnLike = document.createElement('button');
+  cardBtnLike.classList.add('elements__heart');
+  cardBtnLike.type = "button";
+
+  cardElements.append(cardTitle);
+  cardElements.append(cardBtnLike);
+  card.append(cardPhoto);
+  card.append(cardElements);
+  elements.append(card);
+});
+
 function togglePopup() {
   if (!popup.classList.contains('popup_opened')) {
     nameInput.value = profileName.textContent;
